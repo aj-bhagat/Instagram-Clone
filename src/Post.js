c2e1f2b9136769f00d80c5a13cfbd10a3da05894
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-04 01:23:51 +0530

    Post Comp

--- src/Post.js
+++ src/Post.js
@@ -5,14 +5,16 @@ import Avatar from "@material-ui/core/Avatar"
 function Post() {
     return (
         <div className="post">
+            <div className="post_header">
             <Avatar className="post_avatar" alt='aj_bhagat_'
             src="/static/images/avatar/1.jpg"/>
             <h3>Username</h3>
-            {/*header->avatar+username */}
+            </div>
+            
 
             <img className="post_image" src="https://i.pinimg.com/originals/1e/17/f4/1e17f44decace4f57ffa9b5b5d3f1729.png" alt=''></img>
             {/* image */}
-            <h4 className="post_text"><strong>aj_bhagat_</strong>Never Give Up!</h4>
+            <h4 className="post_text"><strong>aj_bhagat_</strong> Never Give Up!</h4>
         </div>
     )
 }

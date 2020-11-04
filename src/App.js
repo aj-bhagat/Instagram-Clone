005546354f9cd83073d222d66a85490db3710f5e
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-05 01:23:36 +0530

    App Comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, {useState} from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+  const[posts, setPosts] = useState([
+    {
+      username: "ajax_bhaga",
+      caption: "Never Give Up!",
+      imageUrl: "https://i.pinimg.com/736x/7e/29/cd/7e29cda50f0f3704ef86c7061612f05b.jpg",
+    },
+    {
+      username: "aj_bhagat_",
+      caption: "Sometimes life is too uncertain to have regrets",
+      imageUrl: "https://i.pinimg.com/originals/88/db/29/88db29b9c4344ef72011df281fb03936.jpg",
+    }
+  ]);
   return (
     <div className="app">
       
@@ -12,9 +25,12 @@ function App() {
           />
       </div>
       <h1>Instagram Clone</h1>
-      <Post/>
-      <Post/>
-      <Post/>
+      {
+        
+      }
+      <Post username="ajax_bhaga" caption="Never Give Up!" imageUrl="https://i.pinimg.com/736x/7e/29/cd/7e29cda50f0f3704ef86c7061612f05b.jpg"/>
+      <Post username="aj_bhagat_" caption="Sometimes life is too uncertain to have regrets" imageUrl="https://i.pinimg.com/originals/88/db/29/88db29b9c4344ef72011df281fb03936.jpg"/>
+      <Post username="bhaga_Aj" caption="If you don’t give up, you’ll get through" imageUrl="https://i.pinimg.com/originals/e6/ac/b4/e6acb49919188014d955cb52f1e4572f.jpg"/>
       <Post/>
     </div>
   );

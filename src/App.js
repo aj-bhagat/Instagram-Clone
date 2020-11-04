294d2927aef95f800b7e4d42ab1168c0aed5e473
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-05 01:32:36 +0530

    App Comp

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
       username: "aj_bhagat_",
       caption: "Sometimes life is too uncertain to have regrets",
       imageUrl: "https://i.pinimg.com/originals/88/db/29/88db29b9c4344ef72011df281fb03936.jpg",
+    },
+    {
+      username: "bhaga_Aj",
+      caption: "If you don’t give up, you’ll get through",
+      imageUrl: "https://i.pinimg.com/originals/e6/ac/b4/e6acb49919188014d955cb52f1e4572f.jpg",
     }
   ]);
   return (
@@ -26,12 +31,11 @@ function App() {
       </div>
       <h1>Instagram Clone</h1>
       {
-        
+        posts.map(post=>(
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
       }
-      <Post username="ajax_bhaga" caption="Never Give Up!" imageUrl="https://i.pinimg.com/736x/7e/29/cd/7e29cda50f0f3704ef86c7061612f05b.jpg"/>
-      <Post username="aj_bhagat_" caption="Sometimes life is too uncertain to have regrets" imageUrl="https://i.pinimg.com/originals/88/db/29/88db29b9c4344ef72011df281fb03936.jpg"/>
-      <Post username="bhaga_Aj" caption="If you don’t give up, you’ll get through" imageUrl="https://i.pinimg.com/originals/e6/ac/b4/e6acb49919188014d955cb52f1e4572f.jpg"/>
-      <Post/>
+      
     </div>
   );
 }

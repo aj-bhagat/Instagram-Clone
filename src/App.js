ff815976f9e923a9851c3994d107c30f414e7bd8
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-08 00:48:07 +0530

    App Comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post';
+import db from './firebase';
 
 function App() {
-  const[posts, setPosts] = useState([
-    {
-      username: "ajax_bhaga",
-      caption: "Never Give Up!",
-      imageUrl: "https://i.pinimg.com/736x/7e/29/cd/7e29cda50f0f3704ef86c7061612f05b.jpg",
-    },
-    {
-      username: "aj_bhagat_",
-      caption: "Sometimes life is too uncertain to have regrets",
-      imageUrl: "https://i.pinimg.com/originals/88/db/29/88db29b9c4344ef72011df281fb03936.jpg",
-    },
-    {
-      username: "bhaga_Aj",
-      caption: "If you don’t give up, you’ll get through",
-      imageUrl: "https://i.pinimg.com/originals/e6/ac/b4/e6acb49919188014d955cb52f1e4572f.jpg",
-    }
-  ]);
+  const[posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot =>{
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    });
+  }, []);
+
   return (
     <div className="app">
       
@@ -31,11 +26,11 @@ function App() {
       </div>
       <h1>Instagram Clone</h1>
       {
-        posts.map(post=>(
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        posts.map(({id, post})=>(
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         ))
       }
-      
+
     </div>
   );
 }

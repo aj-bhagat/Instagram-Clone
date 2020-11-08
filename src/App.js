ee4e046f0b2359cf73535e47c2fe321b68ab4848
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-08 18:08:02 +0530

    App Comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import db from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
-  const[posts, setPosts] = useState([]);
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot =>{
+    db.collection('posts').onSnapshot(snapshot => {
       setPosts(snapshot.docs.map(doc => ({
         id: doc.id,
         post: doc.data()
@@ -15,19 +49,54 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div className="app">
-      
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signup">
+            <center>
+              <img className="app_headerImage"
+                src="https://v2.droproball.com/wp-content/uploads/2020/04/instagram-text-logo.png"
+                alt=''
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
       <div className="app_header">
-          <img className="app_headerImage"
+        <img className="app_headerImage"
           src="https://v2.droproball.com/wp-content/uploads/2020/04/instagram-text-logo.png"
           alt=''
-          />
+        />
       </div>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <h1>Instagram Clone</h1>
       {
-        posts.map(({id, post})=>(
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
 

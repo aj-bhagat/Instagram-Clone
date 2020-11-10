8d7d4253b2c5e86d45c6a0073083d7db660b9c10
aj-bhagat <ajaybhagatm620@gmail.com>
2020-11-11 00:34:54 +0530

    ImageUpload Comp

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,6 +2,7 @@ import { Button } from '@material-ui/core';
 import React, {useState} from 'react';
 import db, {storage} from './firebase';
 import firebase from "firebase";
+import './ImageUpload.css';
 
 function ImageUpload({username}) {
     const[caption, setCaption]= useState('');
@@ -55,10 +56,7 @@ function ImageUpload({username}) {
         )
     }
     return (
-        <div>
-            {/*Caption input... */}
-            {/*File picker image... */}
-            {/*Post/Upload button */}
+        <div className="imageupload">
             <progress value={progress} max="100"/>
             <input typr="text" placeholder="Enter a Caption..." onChange={event => setCaption(event.target.value)} value={caption}/>
             <input type="file" onChange={handleChange}/>
